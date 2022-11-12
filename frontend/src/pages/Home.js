5327d7e250cb3e7981d0427db73fed6ba926cf96
nayani <nayani.dn@gmail.com>
2022-11-12 17:31:10 +0530

    Table colors change

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,13 +4,13 @@ function Home() {
   return (
     <div className='home'>
      <div class="about-section">
-  <h1>About Us Page</h1>
-  <p>A synonym for quality, speed and care, Maga Engineering is one of the most trusted construction </p>
-  <p>companies in South Asia, delivering high-quality buildings and infrastructure for over three decades. 
- From concept to conclusion, Maga continues to work with an array of stakeholders from investors and </p>
-  <p> From concept to conclusion, Maga continues to work with an array of stakeholders from investors and 
-  developers to public agencies and regional communities. As the largest construction company in Sri Lanka, </p>
-  <p>its work has enriched all sectors of the economy, from healthcare and hospitality to transport and water supply.</p>
+  <h1>Sky Light Construction</h1>
+  <p>A synonym for quality, speed and care, Maga Engineering is one of the most trusted construction 
+  companies in South Asia, delivering high-quality buildings and infrastructure for over three decades.</p> 
+ <p>From concept to conclusion, Maga continues to work with an array of stakeholders from investors and 
+  From concept to conclusion, Maga continues to work with an array of stakeholders from investors and 
+  developers to public agencies and regional communities. As the largest construction company in Sri Lanka, 
+  its work has enriched all sectors of the economy, from healthcare and hospitality to transport and water supply.</p>
 
 </div>
     </div>
